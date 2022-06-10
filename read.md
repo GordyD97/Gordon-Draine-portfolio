381cf3a63fcd9624401d9eabaf4c8c8135bb46a9
Turbo Gordz <gordon.draine97@yahoo.com>
2022-06-09 23:36:42 -0400

    fixing avatar image to flex

--- read.md
+++ read.md
@@ -1,4 +1,9 @@
-Read me File !
-![](./assets/images/portfolio.PNG)
+# Gordon Draine IV's Portfolio
 
-Hello, Im Gordon Draine IV I am a novice web developer. 
+## Hello, Im Gordon Draine IV I am a novice web developer.
+
+### This the repository for my portfolio, this is my kick off to a new career! I am so excited to dive deep into the career path of a web developer !
+
+
+
+![](./assets/images/portfolio.PNG)
--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-const list = document.querySelectorAll('.navbarnav');
+const list = document.querySelectorAll('.nav-item');
 function activeLink(){
     list.forEach((item)=>
     item.classList.remove('#active'));
     this.classList.add('#active');
 }
 list.forEach((item)=>
-item.addEventListener('click',activeLink))
+item.addEventListener('mouseover',activeLink))
+
+
+// const navItem = document.querySelector('.nav-item');
+// function activeLink(){
+// navItem.forEach((item) =>
+// item.classList.remove('active'));
+// this.classList.add('active');
+// }
+// list.forEach((item) =>
+// item.addEventListener('click',activeLink ));
